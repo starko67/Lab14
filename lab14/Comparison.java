60c275153de900c653d6a0fa1a415e0529548d5d
starko67 <147971282+starko67@users.noreply.github.com>
2023-10-17 14:08:11 +0300

    Lab14

---
+++ lab14/Comparison.java
@@ -0,0 +1,13 @@
+package lab14;
+import java.util.Scanner;
+public class Comparison {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String invalidString1 = scanner.next();
+        System.out.println(isValidString(invalidString1)); // Должно вернуть false
+    }
+    public static boolean isValidString(String input) {
+        String pattern = "abcdefghijklmnopqrstuv18340";
+        return input.matches(pattern);
+    }
+}
